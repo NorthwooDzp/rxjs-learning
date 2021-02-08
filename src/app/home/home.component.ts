00e4b9d623fdb208562514a90f7cc1700cd665d8
Vladimir Lazarchuk <northwoodzp@gmail.com>
2021-02-08 22:42:26 +0200

    P1 L7

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { fromEvent, interval, Observable, of, timer } from 'rxjs';
+import { catchError, delayWhen, map, retryWhen, shareReplay, tap } from 'rxjs/operators';
 
 
 @Component({
@@ -13,11 +13,27 @@ export class HomeComponent implements OnInit {
 
 
     constructor() {
-
     }
 
-    ngOnInit() {
+    public ngOnInit(): void {
 
+        const interval$ = interval(1000);
+
+        const timer$ = timer(3000, 1000);
+
+        const clicks$ = fromEvent(document, 'click');
+
+        interval$.subscribe(val => {
+            console.log('stream 1 => ', val);
+        })
+
+        timer$.subscribe(val => {
+            console.log('stream 2 => ', val);
+        })
+
+        clicks$.subscribe(ev => {
+            console.log('clicks => ', ev);
+        })
 
     }
 
