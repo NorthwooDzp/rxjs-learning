74893c74ea1204233a722233ea0a5d109e6da3a5
Vladimir Lazarchuk <northwoodzp@gmail.com>
2021-03-13 21:40:45 +0200

    P2 L6 concat() operator

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { concat, Observable, of } from 'rxjs';
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+    selector: 'about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        const source1$: Observable<number> = of(1, 2, 3);
+        const source2$: Observable<number> = of(4, 5, 6);
+        const source3$: Observable<number> = of(7, 8, 9);
+        concat(source1$, source2$, source3$).subscribe(console.log);
+    }
 
 }
